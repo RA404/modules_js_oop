968d090761052ed22041b52c0d0300340c5e0c33
Repin Andrey <RA404@yandex.ru>
2020-03-22 21:26:35 +0500

    config entry and output

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+<script src="js/FormValidator.js"></script>
+<script src="js/Card.js"></script>
+<script src="js/CardList.js"></script>
+<script src="js/PopupEditInfo.js"></script>  
+<script src="js/PopupImage.js"></script>  
+<script src="js/PopupNewPlace.js"></script> 
+<script src="js/UserInfo.js"></script>   
+
+<script src="js/Api.js"></script>
+<script src="js/script.js"></script>
+*/
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path'); // подключаем path к конфигу вебпак
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: { main: './src/js/script.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
