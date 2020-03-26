e671b2b7ab022c32f08d741b5eb0e770672f17ca
Repin Andrey <RA404@yandex.ru>
2020-03-26 22:27:16 +0500

    trying to fix css error

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
+const StyleLoader = require('style-loader');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -23,7 +24,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader'] // добавили минификацию CSS
+                use: [(isDev ? StyleLoader.loader : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader'] // добавили минификацию CSS
             },
             {
                 test: /\.(png|jpg|gif|ico|svg)$/,
@@ -51,6 +52,7 @@ module.exports = {
         new MiniCssExtractPlugin({ // 
             filename: 'style.[contenthash].css',
         }),
+        new StyleLoader(),
         new HtmlWebpackPlugin({
             inject: false,
             template: './src/index.html',
@@ -59,7 +61,14 @@ module.exports = {
         new webpack.DefinePlugin({
             'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         }),
-        new OptimizeCssAssetsPlugin(),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                    preset: ['default'],
+            },
+            canPrint: true
+        }), 
         new WebpackMd5Hash()
     ]
 };
